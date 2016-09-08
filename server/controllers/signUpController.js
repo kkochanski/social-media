bd0b903ca261ba69a613c8201d9fde599915a455
kkochanski <kf.kochanski@gmail.com>
2016-09-08 20:22:56 +0200

    Simplify promises chaines in backend
    Resolves #2

--- server/controllers/signUpController.js
+++ server/controllers/signUpController.js
@@ -29,9 +29,7 @@ exports.signUp = (request, reply) => {
             toSave.password = bcrypt.hashSync(toSave.password, 10);
             const user = new userModel(toSave);
 
-            return user.save().catch(err => {
-                return Promise.reject({code: 'db-error', msg: err});
-            }).then(createdUser => {
+            return user.save().then(createdUser => {
                 const confirmationUrl = request.server.app.serverUrl + '/sign-up/confirmation/' + user.confirmationToken,
                     mailOptions = {
                         from: request.server.app.di.container.configLoader.get('/email/config/from'),
@@ -40,31 +38,23 @@ exports.signUp = (request, reply) => {
                         html: '<h3>Hello ' + createdUser.firstName + '</h3> <div>Thank you for your registration. To end up your sign up process, please confirm your e-mail address by click this url: <a href="' + confirmationUrl + '/' + '">Confirmation URL</a> </div>'
                     };
 
-                return request.server.app.di.container.mailTransporter.sendMail(mailOptions);
-            }).catch(err => {
-                if(err.code !== 'db-error') {
-                    err = {code: 'mail-error', msg: err};
-                    userModel.remove({'_id': user['_id']}, (err) => { /* Remove already created user. Callback is needed, without that, removal won't work. */
-                        console.log('Error while removing user on send mail fail: ' + err);
+                return request.server.app.di.container.mailTransporter.sendMail(mailOptions).catch(err => {
+                    userModel.remove({'_id': user['_id']}, (dbErr) => { /* Remove already created user. Callback is needed, without that, removal won't work. */
+                        if(dbErr) {
+                            console.log('Error while removing user on send mail fail: ' + dbErr);
+                        }
                     });
-                }
-                return Promise.reject(err);
-            }).then(() => {
-                return Promise.resolve();
-            }).catch( (err) => {
-                return Promise.reject(err);
-            });
-        }).catch(function (err) {
-            return Promise.reject(err);
-        })
+                    return Promise.reject(err);
+                });
+            })
+        });
     }).catch(error => {
         if(typeof error === 'object' && error.isBoom) {
             return error;
         }
 
-        const Boom = require('boom'),
-            errorRowLog = 'Error in \'signUp\' action.';
-        request.server.app.di.container.logger.error(errorRowLog, error);
+        const Boom = require('boom');
+        request.server.app.di.container.logger.error('Error in \'signUp\' action, in ' + __filename, error);
 
         return Boom.notAcceptable('Unexpected error occurred. Please try again or contact with admin.');
     }).then(response => {
@@ -87,36 +77,33 @@ exports.signUp = (request, reply) => {
 exports.signUpConfirmation = (request, reply) => {
 
     const userModel = request.server.app.di.container.userModel,
-        Promise = require('bluebird');
+        Promise = require('bluebird'),
+        Boom = require('boom');
 
     userModel.then(userModel => {
         return userModel.findOne({'confirmationToken': request.payload.token, 'confirmedAt': null}).then(user => {
             if(user === null) {
                 return userModel.findOne({'confirmationToken': request.payload.token}).then(function(user) {
-                    const msg = user !== null ? 'Your e-mail is already confirmed.' : 'Can\'t find user with given token. Your token is invalid.',
-                        err = {code: 'internal', msg: msg};
-
-                    return Promise.reject(err);
+                    if(user !== null) {
+                        return Boom.notAcceptable('Your e-mail is already confirmed.')
+                    } else {
+                        return Boom.notAcceptable('Can\'t find user with given token. Your token is invalid..')
+                    }
                 })
             }
 
             return user.update({'confirmedAt': new Date()}).then((err, numberAffected, rawResponse) => {
                 return Promise.resolve({'email': user.email, 'firstName': user.firstName, 'lastName': user.lastName, 'confirmationToken': user.confirmationToken});
-            }).catch(err => {
-                return Promise.reject({code: 'db-error', msg: err});
-            });
-        }).catch(err => {
-            return Promise.reject(err.code === 'internal' ? err : {code: 'default-error', msg: err});
-        });
+            })
+        })
     }).catch(err => {
-        const Boom = require('boom');
-        if(err.code === undefined) {
-            err = {code : 'default-error', msg: err};
+        if(typeof err === 'object' && err.isBoom) {
+            return err;
         }
 
-        request.server.app.di.container.logger.error('Error in \'signUpConfirmation\' action', err);
+        request.server.app.di.container.logger.error('Error in \'signUpConfirmation\' action, in ' + __filename, err);
 
-        return Boom.notAcceptable(err.code === 'internal' ? err.msg : 'Unexpected error occurred. Please try again or contact with admin.');
+        return Boom.notAcceptable('Unexpected error occurred. Please try again or contact with admin.');
     }).then(response => {
         return reply(response);
     });
